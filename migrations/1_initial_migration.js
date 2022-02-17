0163249776cd414366644dea3a9cf72082fe86d8
Steve Boyer <stephen.a.boyer@gmail.com>
2022-02-17 12:53:20 -0600

    Convert user balance to human-readable format

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -10,6 +10,7 @@ module.exports = async function(deployer) {
   await tokenMock.mint(
     '0xfa7A3d6eC5Bc1e722aF8Ee6c5ad227a708907Ae1',
     // The amount = 1000 + 18 zeros (because the token has 18 decimals)
+    // When you fetch and console.log(balance.toString()) it will still show as the below number, but the actual balance is 1000
     // You can do this with helper or web3.js but sometimes it is best to be explicit in migrations
     '1000000000000000000000'
   )
